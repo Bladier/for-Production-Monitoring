a3bba7abfac416275d8571cdbb106f172cd36ae7
Bladier <misionaellie@gmail.com>
2016-12-17 16:53:32 -0800

    update

--- PriProMonitoring/PriProMonitoring/Forms/frmImportIMDD.Designer.vb
+++ PriProMonitoring/PriProMonitoring/Forms/frmImportIMDD.Designer.vb
@@ -22,8 +22,124 @@ Partial Class frmImportIMDD
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lvIMD = New System.Windows.Forms.ListView()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.lblFilename = New System.Windows.Forms.Label()
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.ofdIMD = New System.Windows.Forms.OpenFileDialog()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'lvIMD
+        '
+        Me.lvIMD.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvIMD.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.lvIMD.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvIMD.FullRowSelect = True
+        Me.lvIMD.GridLines = True
+        Me.lvIMD.Location = New System.Drawing.Point(12, 68)
+        Me.lvIMD.Name = "lvIMD"
+        Me.lvIMD.Size = New System.Drawing.Size(530, 364)
+        Me.lvIMD.TabIndex = 5
+        Me.lvIMD.UseCompatibleStateImageBehavior = False
+        Me.lvIMD.View = System.Windows.Forms.View.Details
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.Location = New System.Drawing.Point(12, 31)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 31)
+        Me.btnBrowse.TabIndex = 4
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'lblFilename
+        '
+        Me.lblFilename.AutoSize = True
+        Me.lblFilename.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFilename.Location = New System.Drawing.Point(12, 12)
+        Me.lblFilename.Name = "lblFilename"
+        Me.lblFilename.Size = New System.Drawing.Size(64, 16)
+        Me.lblFilename.TabIndex = 6
+        Me.lblFilename.Text = "Filename"
+        '
+        'btnImport
+        '
+        Me.btnImport.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnImport.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImport.Location = New System.Drawing.Point(386, 438)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(75, 31)
+        Me.btnImport.TabIndex = 7
+        Me.btnImport.Text = "&Import"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(467, 438)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 31)
+        Me.btnCancel.TabIndex = 8
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'ofdIMD
+        '
+        Me.ofdIMD.Filter = "Excel 2003|*.xls|Excel 2007|*.xlsx"
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ItemCode"
+        Me.ColumnHeader1.Width = 122
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Description"
+        Me.ColumnHeader2.Width = 144
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "PaperCut"
+        Me.ColumnHeader3.Width = 134
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "QTY"
+        Me.ColumnHeader4.Width = 173
+        '
+        'frmImportIMDD
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "frmImportIMDD"
+        Me.ClientSize = New System.Drawing.Size(549, 481)
+        Me.Controls.Add(Me.lvIMD)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.lblFilename)
+        Me.Controls.Add(Me.btnImport)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Name = "frmImportIMDD"
+        Me.Text = "Import IMD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lvIMD As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents lblFilename As System.Windows.Forms.Label
+    Friend WithEvents btnImport As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents ofdIMD As System.Windows.Forms.OpenFileDialog
 End Class
--- PriProMonitoring/PriProMonitoring/Forms/frmImportIMDD.vb
+++ PriProMonitoring/PriProMonitoring/Forms/frmImportIMDD.vb
@@ -1,3 +1,71 @@
-﻿Public Class frmImportIMDD
+﻿Imports Microsoft.Office.Interop
+Public Class frmImportIMDD
+    Dim ht_ImportedItems As New Hashtable
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowse.Click
+        ofdIMD.ShowDialog()
 
+        Dim fileName As String = ofdIMD.FileName
+        Dim isDone As Boolean = False
+
+        If fileName = "" Then Exit Sub
+        lblFilename.Text = fileName
+        ClearFields()
+
+        'Load Excel
+        Dim oXL As New Excel.Application
+        Dim oWB As Excel.Workbook
+        Dim oSheet As Excel.Worksheet
+
+        oWB = oXL.Workbooks.Open(fileName)
+        oSheet = oWB.Worksheets(1)
+
+        Dim MaxColumn As Integer = oSheet.Cells(1, oSheet.Columns.Count).End(Excel.XlDirection.xlToLeft).column
+        Dim MaxEntries As Integer = oSheet.Cells(oSheet.Rows.Count, 1).End(Excel.XlDirection.xlUp).row
+
+        Dim checkHeaders(MaxColumn) As String
+        For cnt As Integer = 0 To MaxColumn - 1
+            checkHeaders(cnt) = oSheet.Cells(1, cnt + 1).value
+        Next : checkHeaders(MaxColumn) = oWB.Worksheets(1).name
+
+
+        Me.Enabled = False
+        For cnt = 2 To MaxEntries
+            Dim ImportedItem As New ItemData
+            With ImportedItem
+                .ItemCode = oSheet.Cells(cnt, 1).Value
+                .Load_ItemCode()
+
+                .Description = oSheet.Cells(cnt, 2).Value
+                .Barcode = oSheet.Cells(cnt, 3).Value
+                .Category = oSheet.Cells(cnt, 4).Value
+                .SubCategory = oSheet.Cells(cnt, 5).Value
+                .UnitofMeasure = oSheet.Cells(cnt, 6).Value
+                .UnitPrice = If(Not IsNumeric(oSheet.Cells(cnt, 7).Value), 0, oSheet.Cells(cnt, 7).Value)
+                .SalePrice = If(Not IsNumeric(oSheet.Cells(cnt, 8).Value), 0, oSheet.Cells(cnt, 8).Value)
+                
+
+            End With
+
+            'AddItems(ImportedItem)
+        Next
+        Me.Enabled = True
+        isDone = True
+
+
+unloadObj:
+        'Memory Unload
+        oSheet = Nothing
+        oWB = Nothing
+        oXL.Quit()
+        oXL = Nothing
+
+        fileName = ""
+        If isDone Then MsgBox("Item Loaded", MsgBoxStyle.Information)
+    End Sub
+
+
+    Private Sub ClearFields()
+        ht_ImportedItems.Clear()
+        lvIMD.Items.Clear()
+    End Sub
 End Class
